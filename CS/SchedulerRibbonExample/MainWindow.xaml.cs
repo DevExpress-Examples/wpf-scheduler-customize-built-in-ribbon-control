e36c481b70b49d409ea8851f3dcd4eabe884a30d
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/SchedulerRibbonExample/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using DevExpress.Xpf.Core;
+using DevExpress.Xpf.Ribbon;
+using System.Windows;
+
+namespace DXSchedulingRibbon {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : DXRibbonWindow {
+        public MainWindow() {
+            InitializeComponent();
+        }
+        #region #About_ItemClick
+        void About_ItemClick(object sender, DevExpress.Xpf.Bars.ItemClickEventArgs e) {
+            DXMessageBox.Show(@"This example demonstrates how to customize the WPF Scheduler's integrated ribbon UI. Use the Scheduler's RibbonActions collection to create, remove or modify ribbon elements.",
+                "Scheduler Ribbon Example", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+        #endregion #About_ItemClick
+    }
+}
