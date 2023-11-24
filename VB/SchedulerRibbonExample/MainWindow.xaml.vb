484f3a92f7d59412d715819c9e0865e7ee7d28a1
DevExpressExampleBot <fakeEmail>
2023-11-24 18:16:14 +0400

    Source auto update [skip ci]

--- VB/SchedulerRibbonExample/MainWindow.xaml.vb
+++ VB/SchedulerRibbonExample/MainWindow.xaml.vb
@@ -1,21 +1,20 @@
-﻿Imports DevExpress.Xpf.Core
-Imports DevExpress.Xpf.Ribbon
+Imports DevExpress.Xpf.Core
 Imports System.Windows
 
 Namespace DXSchedulingRibbon
+
     ''' <summary>
     ''' Interaction logic for MainWindow.xaml
     ''' </summary>
-    Partial Public Class MainWindow
-        Inherits DXRibbonWindow
+    Public Partial Class MainWindow
+        Inherits ThemedWindow
 
         Public Sub New()
-            InitializeComponent()
+            Me.InitializeComponent()
         End Sub
-        #Region "#About_ItemClick"
+
         Private Sub About_ItemClick(ByVal sender As Object, ByVal e As DevExpress.Xpf.Bars.ItemClickEventArgs)
             DXMessageBox.Show("This example demonstrates how to customize the WPF Scheduler's integrated ribbon UI. Use the Scheduler's RibbonActions collection to create, remove or modify ribbon elements.", "Scheduler Ribbon Example", MessageBoxButton.OK, MessageBoxImage.Information)
         End Sub
-        #End Region ' #About_ItemClick
     End Class
 End Namespace
